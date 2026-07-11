e12fc3b621adf1a95fd56ad70eabb188ae0395d1
Berkan KARA <86982736+Berkankk@users.noreply.github.com>
2026-07-11 14:31:05 +0300

    feat : redux toolkit kütüphensi yüklendi

---
+++ Client/src/Pages/counter/counterSlice.ts
@@ -0,0 +1,34 @@
+import { PayloadAction } from './../../../node_modules/@reduxjs/toolkit/src/createAction';
+import { createSlice } from "@reduxjs/toolkit"
+
+export interface CounterState{
+    value : number
+}
+
+const initialState : CounterState = {
+    value : 0,
+}
+
+export const counterSlice = createSlice({
+    name : "counter",
+    initialState,
+    reducers:{
+        increment:(state) => {
+            state.value +=1;
+        },
+        decrement:(state) => {
+            state.value -=1;
+        },
+        incrementbyAmount:(state, action: PayloadAction<number>) => {
+            state.value += action.payload
+        }
+    }
+})
+
+export const {increment, decrement,incrementbyAmount } = counterSlice.actions;
+
+
+
+
+
+
--- Client/src/main.tsx
+++ Client/src/main.tsx
@@ -4,9 +4,15 @@ import './index.css'
 import {  RouterProvider } from 'react-router'
 import { router } from './Router/Route.tsx'
 import { CartContextProvider } from './Context/CartContext.tsx'
+import { Provider } from 'react-redux'
+import { store } from './store/store.ts'
 
 createRoot(document.getElementById('root')!).render(
-    <CartContextProvider>
-    <RouterProvider router={router} />
-    </CartContextProvider>
+    //Bunun altında ki tüm componentlere erişecek 
+    <Provider store={store}> 
+        <CartContextProvider>
+            <RouterProvider router={router} />
+        </CartContextProvider>
+    </Provider>
+
 )
